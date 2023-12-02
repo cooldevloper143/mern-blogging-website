8e55bd9b1d134b70d9c003cbf87b2a84d102da1e
aisarthak <aisarthak2@gmail.com>
2023-12-03 01:04:28 +0530

    routes setup complleted
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import Navbar from "./components/navbar.component";
+
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component.jsx";
 
 const App = () => {
     return (
-        <Navbar />
+        <Routes>
+            <Route path="/" element={<Navbar />} >
+                <Route path="/signin" element={<h1>signin page</h1>} />
+                <Route path="/signup" element={<h1>signup page</h1>} />
+            </Route>
+
+        </Routes>
     )
 }
 
-export default App;
+export default App; 
--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,5 +1,5 @@
 import logo from '../imgs/logo.png'
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import React, { useState } from 'react';
 
 const Navbar = () => {
@@ -8,30 +8,43 @@ const Navbar = () => {
 
 
     return (
-        <nav className="navbar">
 
-            <Link to={'/'} className='flex-none w-10'>
+        <>
 
-                <img src={logo} alt="" className='flex-none w-10' />
-            </Link>
-            <div className={'absolute bg-white w-full left-0 top-full  mt-0 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show  ' + (searchboxvisibility ? 'show' : 'hide')}>
-                <input type="text"
-                    placeholder='Search'
-                    className='w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12' />
+            <nav className="navbar">
 
-                <i className="fi fi-rr-search absolute right-[10%] md pointer-events-none md-left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey" ></i>
-            </div>
-            <div className="flex items-center gap-3 md:gap-6 ml-auto  ">
-                <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center'
-                    onClick={() => setsearchboxvisibility(currentVal => !currentVal)}
-                >
-                    <i className="fi fi-rr-search text-xl   "></i>
-                </button>
-                <Link to={'/editor'} className='hidden md:flex items-center gap-3 text-dark-grey hover:text-primary'>
+                <Link to={'/'} className='flex-none w-10'>
 
+                    <img src={logo} alt="" className='flex-none w-10' />
                 </Link>
-            </div>
-        </nav>
+                <div className={'absolute bg-white w-full left-0 top-full  mt-0 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show  ' + (searchboxvisibility ? 'show' : 'hide')}>
+                    <input type="text"
+                        placeholder='Search'
+                        className='w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12' />
+
+                    <i className="fi fi-rr-search absolute right-[10%] md pointer-events-none md-left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey" ></i>
+                </div>
+                <div className="flex items-center gap-3 md:gap-6 ml-auto  ">
+                    <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center'
+                        onClick={() => setsearchboxvisibility(currentVal => !currentVal)}
+                    >
+                        <i className="fi fi-rr-search text-xl   "></i>
+                    </button>
+                    <Link to={'/editor'} className='hidden md:flex gap-2 link'>
+                        <i class="fi fi-rr-file-edit"></i>
+                        <p>write</p>
+                    </Link>
+
+                    <Link to={'/signin'} className='btn-dark py-2'>
+                        <p>sign in</p>
+                    </Link>
+                    <Link to={'/signup'} className='btn-light hidden md:block py-2'>
+                        <p>sign up</p>
+                    </Link>
+                </div>
+            </nav>
+            <Outlet />
+        </>
     )
 }
 
