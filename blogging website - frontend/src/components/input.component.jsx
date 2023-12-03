2cf6e72c6fe66711939d08beb34b526519a038c6
aisarthak <aisarthak2@gmail.com>
2023-12-03 10:34:42 +0530

    show hide password icon dynamically
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- blogging website - frontend/src/components/input.component.jsx
+++ blogging website - frontend/src/components/input.component.jsx
@@ -1,10 +1,34 @@
-const InputBox = ({ name, type, id, value, placeholder }) => {
+import React, { useState } from "react";
+
+
+
+const InputBox = ({ name, type, id, value, placeholder, icon }) => {
+    const [passwordvidible, setPasswordvidible] = useState(false);
+
     return (
         <div className="relative w-[100%] mb-4">
-            <input type={type} name={name} id={id} value={value} placeholder={placeholder} className="w-[100%] h-[50px] bg-white px-4 py-2 rounded-lg border-2 border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent" />
+            <input
+                type={
+                    type == "password" ? (passwordvidible ? "text" : "password") : type
+                }
+                name={name}
+                id={id}
+                placeholder={placeholder}
+                defaultValue={value}
+                className="input-box"
+            />
+            <i className={"fi " + icon + " input-icon"}></i>
 
+            {type == "password" ? (
+                <i
+                    className={"fi fi-sr-eye" + (!passwordvidible ? "-crossed" : "")+" input-icon left-[auto] right-4 cursor-pointer"}
+                    onClick={() => setPasswordvidible((currentVal) => !currentVal)}
+                ></i>
+            ) : (
+                ""
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default InputBox;
--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -7,8 +7,31 @@ const UserAuthForm = ({ type }) => {
         <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
           {type == "signin" ? "welcome back" : "Join up today"}
         </h1>
+        {/* renderedn input box with props so it will change dyanmically  */}
+        {type != "signin" ? <InputBox
+          name="full name"
+          type="text"
+          // id="full name"
+          placeholder="full name"
+          icon={"fi-sr-user"}
 
-        {type != "signin" ? <InputBox/> : null}
+        /> : null}
+        <InputBox
+          name="email"
+          type="email"
+          // id="full name"
+          placeholder="email"
+          icon={"fi-sr-envelope"}
+
+        />
+        <InputBox
+          name="password"
+          type="password"
+          // id="full name"
+          placeholder="password"
+          icon={"fi-sr-key"}
+
+        />
       </form>
     </section>
   );
