8c8d66c6a7c966dcfffe06c31ab7159f281f6954
aisarthak <aisarthak2@gmail.com>
2023-12-03 14:01:48 +0530

    mongodb connection setup
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+
+
+
 
 const server = express();
+let PORT = process.env.PORT || 3001;
 
-let PORT = process.env.PORT || 3000;
+try {
+    mongoose.connect("mongodb+srv://aisarthak2:45YuS7tdMClDDgSd@cluster0.mj3b3jf.mongodb.net")
+    console.log("Connected to MongoDB successfully✅✅✅😎") 
+    
+} catch (error) {
+    console.log("Unable to connect to MongoDB❌❌❌😥", error)
+    process.exit(1);
+}
 
-server.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-});
+
+server.listen(PORT, () => console.log(`\n**===** Listening on port ${PORT} **===**\n`));
+
+
+
+
